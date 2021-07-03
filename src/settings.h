684d43f9a8ab8a156d74de318cbe6889817d1eb9
Simmessa <simmessa@gmail.com>
2021-07-03 18:30:44 +0200

    gitignore

--- src/settings.h
+++
@@ -1,6 +0,0 @@
-// Update these with values suitable for your network.
-
-const char ssid[] = "YOUR_SSID"; // your network SSID (name)
-const char pass[] = "YOUR_WIFI_PASSWORD";  // your network password
-
-const char mqtt_server[] = "YOUR_MQTT_SERVER_IP";
